823400b0aca0236d17949f0c73eba7b14046217b
tushukk <115683689+tushukk@users.noreply.github.com>
2023-07-12 18:35:03 +0530

    Create Before_overidefun.kt

---
+++ Before_overidefun.kt
@@ -0,0 +1,7 @@
+lateinit var btnShow:Button
+    lateinit var tv0: TextView
+    var num=0
+    lateinit var btnWithItem : Button
+    lateinit var btnWithMItem : Button
+    var items= arrayOf("dog","cat","rat")
+    var booleanItems= booleanArrayOf(false,false,false)
